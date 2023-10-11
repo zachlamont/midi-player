76ed35bb62cce28dec7c0987afe975ffbef5644d
zachlamont <zachlamont1@gmail.com>
2023-10-11 22:14:18 +1000

    Add MidiDropZone

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import MidiDropZone from "./components/MidiDropZone";
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        <h1>Midi Player</h1>
+        <MidiDropZone />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/MidiDropZone.jsx
@@ -0,0 +1,57 @@
+import React, { useState, useCallback } from "react";
+import { Midi } from "@tonejs/midi";
+
+function MidiDropZone(props) {
+  const [midiFiles, setMidiFiles] = useState([]);
+
+  const handleDrop = useCallback(async (event) => {
+    event.preventDefault();
+    const files = event.dataTransfer.files;
+
+    const midiFilePromises = Array.from(files)
+      .filter((file) => file.type === "audio/midi")
+      .map((file) =>
+        file.arrayBuffer().then((data) => ({
+          data: new Midi(data),
+          name: file.name,
+          active: false,
+        }))
+      );
+
+    const newMidiFiles = await Promise.all(midiFilePromises);
+    setMidiFiles((prevMidiFiles) => [...prevMidiFiles, ...newMidiFiles]);
+  }, []);
+
+  const handleCardClick = (index) => {
+    setMidiFiles((prevFiles) =>
+      prevFiles.map((file, i) => ({
+        ...file,
+        active: i === index,
+      }))
+    );
+  };
+
+  return (
+    <div className="midi-dropzone-container">
+      <div
+        className="midi-dropzone"
+        onDragOver={(e) => e.preventDefault()}
+        onDrop={handleDrop}
+      >
+        Drop midi file here
+      </div>
+      {midiFiles.map((file, index) => (
+        <div
+          key={index}
+          className={`midi-card ${file.active ? "active" : ""}`}
+          onClick={() => handleCardClick(index)}
+        >
+          {file.name}
+        </div>
+      ))}
+      {props.children}
+    </div>
+  );
+}
+
+export default MidiDropZone;
